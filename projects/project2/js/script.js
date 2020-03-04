05cd6fe1480cef959cb7fb01f66983a92c06339b
Timothy <tm.serrano@icloud.com>
2020-03-04 12:20:10 -0500

    added annyang voice command, sub button, and credit card array on click

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,101 +1,64 @@
+/********************************************************************
+
+Glitch TV Slaves
+Timothy Serrano
+
+If you're familiar with the huge streaming platform Twitch, then this name/concept should be of no surprise.
+Twitch is a platform where content creators live stream their content and interact with viewers in real time.
+Viewers are often encouraged to donate to their favourite streamers in order for them to continue to live off of the
+platform. It is a rough platform where you must dance for your audience in order to live. This project is dedicated
+to be a brutalist satire presentation of how the twitch life can be hard, as well as what viewing twitch as a curious
+youth can feel like.
+
+*********************************************************************/
+
 "use strict";
 
 $(document).ready(setup);
 
-//variables for our secrets
-let secretsFound = 0;
-let totalSecrets;
-
-//our garbage bin
-let $bin;
-//our crap words
-let $found;
+//Our Array to generate credit cards
+let creditImageArray = ['creditcard1.png', 'creditcard2.png', 'creditcard3.png']
 
 
 function setup() {
-  //will re-hide spans every 500 frames
-  setInterval(update, 500);
-  start.currentTime=0;
-  start.play();
-  console.log($('.secret').length);
-
-  //where we'll drop our crap words
-  $bin = $('img');
-  // Make it droppable
-  $bin.droppable({
-    // The drop option specifies a function to call when a drop is completed
-    drop: onDrop
-  });
-
-  $found = $('.secret');
-  $found.draggable(
-
-  );
-
-  //will call our spanClicked function when clicking on specific boxes
-  $('.secret').on('click', spanClicked);
-
-  totalSecrets = $('.secret').length;
-  $('#totalSecrets').text(totalSecrets);
-  //adds an event to mouseover for all secrets
-  $('.secret').on('mouseover', discoveredSecrets);
+  //our subscribe button
+  subscribe();
+  //giving our subscribe button an onclick function
+  let $subscribed = $('#subscribed');
+  $subscribed.on('click', creditCard)
 }
 
-function update(){
+function creditCard() {
+  //console log to verify our button clicks
+  console.log('clicked!');
 
-  //will call on the span removing on click
-  $('span').mousedown(spanClicked);
+  //array to generate credit cards on clicking the subscribe button
+  for (let i = 0; i < 15; i++) {
+
+    let $creditImage = $("<img>");
+
+    let randomIndex = Math.floor(Math.random() * creditImageArray.length)
+    $creditImage.attr("src", "../assets/images/" + creditImageArray[randomIndex]);
+    $creditImage.css("top", "10px");
+    let randomX = Math.random() * window.innerWidth;
+    $creditImage.css("left", randomX + "px");
+    $creditImage.addClass('credit');
+    $('body').append($creditImage);
+  }
 }
 
-//function for deleting our dragged elements
-function onDrop(event, ui) {
+//annyang function for the subscribe feature
 
-  //deletes words
-  ui.draggable.remove();
-  //plays burn track on deletion
-  burn.currentTime=0;
-  burn.play();
+function subscribe() {
+  if (annyang) {
+    let subscribe = {
+      'I subscribed': function() {
+        console.log('THANK YOU FOR SUBSCRIBING WOW');
+      }
+    };
+    annyang.addCommands(subscribe);
+    annyang.start();
+  }
 
-  $(this).attr('h1');
-  //adds points to words deleted
-  secretsFound += 1;
-  //shows the total amount of secrets found
-  $('#foundSecrets').text(secretsFound);
+
 }
-
-//draggable elements
-function spanClicked(){
-  $(this).removeClass('revealed');
-  $(this).addClass('redacted');
-
-  selected.currentTime=0;
-  selected.play();
-}
-
-//function used to display and count up all the total secrets on page
-function discoveredSecrets(){
-  $(this).addClass('found');
-  $(this).off('mouseover');
-  beep.currentTime = 0;
-  beep.play();
-}
-
-
-
-window.onload = function () {
-    let deathCount = 30 * 1,
-        display = document.querySelector('#time');
-    startTimer(deathCount, display);
-};
-/********************************************************************
-
-The Endless Paradox
-Timothy Serrano
-
-Welcome to the paradox prison. As the gong rings, you must burn as many words as the paradox asks for.
-If you do not burn enough words by the time the timer hits 0, then the gong will ring once more, and time will reset.
-Aka, if you don't drag as many descovered words into the flame, the game refreshes.
-
-Here's the catch though: there is no 13th word. There are only 12.
-
-*********************************************************************/
